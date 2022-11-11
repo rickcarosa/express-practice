05d05afc46ee7338c43d848754eb1d3c85756f36
Ricardo Carosa <ricardo.j.carosa@pwc.com>
2022-11-11 15:41:20 -0500

    create api project for movie and search

---
+++ movieApi/app.js
@@ -0,0 +1,71 @@
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const helmet = require("helmet");
+
+var indexRouter = require("./routes/index");
+const movieRouter = require("./routes/movie");
+const searchRouter = require("./routes/search");
+
+var app = express();
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: [
+        "'self'",
+        "'unsafe-inline'",
+        "maxcdn.bootstrapcdn.com",
+        "ajax.googleapis.com",
+      ],
+      styleSrc: ["'self'", "maxcdn.bootstrapcdn.com"],
+      imgSrc: ["'self'", "image.tmdb.org"],
+    },
+  })
+);
+
+app.use((req, use, next) => {
+  // cut off response if api key is bad
+  if (req.query.api_key != 123456789) {
+    // unauthorized = 401, express will send a 200 by default so we need to send the right error code
+    res.status(401);
+    res.json("Invalid API Key");
+  } else {
+    next();
+  }
+});
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+
+app.use("/movie", movieRouter);
+app.use("/search", searchRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
