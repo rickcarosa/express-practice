295e8558737043fb4fd3e106d3e0b0f357623218
Ricardo Carosa <ricardo.j.carosa@pwc.com>
2022-11-11 09:21:19 -0500

    add review text

--- myNewExpressSite/routes/index.js
+++ myNewExpressSite/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // how to set headers
+  const date = new Date(1969, 6, 20);
+  res.set("Date", date);
+  res.set("Content-Type", "text/plain");
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
